ef0700144797d89dad5ad45c5dab1893bda06c84
Oleksandr Korobov <korobov.alex@gmail.com>
2018-05-27 09:44:51 +0300

    Some related theory

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='simple_np',
+    version='0.1',
+    packages=[''],
+    url='https://github.com/korobool/simple_np',
+    license='MIT License',
+    author='oleksandr',
+    author_email='korobov.alex@gmail.com',
+    description='Noun phrases extractor that works on a broken syntax better then solutions that expect text to be '
+                'consistent. '
+)
