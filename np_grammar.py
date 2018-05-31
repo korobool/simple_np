e296bec3cf570f87cc9bcf6342dd0aabb302f279
Svetozar Lashin <niekas33@gmail.com>
2018-05-31 11:56:54 +0300

    added comments, deleted doublets

--- np_grammar.py
+++ np_grammar.py
@@ -1,25 +1,23 @@
 np_rules = {
-    ('CD', 'JJ', 'NN'),
-    ('CD', 'NN'),
-    ('CD', 'RB', 'JJ', 'NN'),
-    ('DT', 'CD', 'JJ', 'NN'),
-    ('DT', 'CD', 'NN'),
-    ('DT', 'CD', 'RB', 'JJ', 'NN'),
-    ('DT', 'JJ', 'CD', 'JJ', 'NN'),
-    ('DT', 'JJ', 'CD', 'NN'),
-    ('DT', 'JJ', 'CD', 'RB', 'JJ', 'NN'),
-    ('DT', 'JJ', 'JJ', 'NN'),
-    ('DT', 'JJ', 'NN'),
-    ('DT', 'JJ', 'RB', 'JJ', 'NN'),
-    ('DT', 'NN'),
-    ('DT', 'RB', 'JJ', 'NN'),
-    ('DT', 'RB', 'VB', 'NN'),
-    ('DT', 'VB', 'NN'),
-    ('JJ', 'CD', 'JJ', 'NN'),
-    ('JJ', 'CD', 'NN'),
-    ('JJ', 'CD', 'RB', 'JJ', 'NN'),
-    ('JJ', 'JJ', 'NN'),
-    ('JJ', 'NN'),
-    ('JJ', 'RB', 'JJ', 'NN'),
-    ('NN',)
+    ('CD', 'JJ', 'NN'), # three French names # I found only three French names among these thirteen travellers.
+    ('CD', 'NN'), # Three Amigos # Three Amigos is a 1986 American western comedy film.
+    ('CD', 'RB', 'JJ', 'NN'), # 5 very good reasons #  Here are 5 very good reasons to keep your hands off marine life!
+    ('DT', 'CD', 'JJ', 'NN'), # a 2012 presidential election # Do we really even need a 2012 presidential election?
+    ('DT', 'CD', 'NN'), # The Seven Sisters # The Seven Sisters is a novel by Margaret Drabble.
+    ('DT', 'CD', 'RB', 'JJ', 'NN'),  # the four very young visitors # Among the four very young visitors, however, there was one man of about thirty.
+    ('DT', 'JJ', 'CD', 'JJ', 'NN'), # the first three administrative divisions # The main line ran 180 miles to Nanoon, encompassing the first three administrative divisions.
+    ('DT', 'JJ', 'CD', 'NN'), # the first three days # Were the first three days of creation literal days? 
+    ('DT', 'JJ', 'CD', 'RB', 'JJ', 'NN'), # the first five carefully constructed minutes # You'll think again about the off-key interactions between Martin and Elizabeth in the first five carefully constructed minutes.
+    ('DT', 'JJ', 'NN'), # a nasty experience # You've had a nasty experience. 
+    ('DT', 'JJ', 'RB', 'JJ', 'NN'), # the first really impressive thing # My mum tells me that the first really impressive thing I made was a blue Play-Doh kangaroo. 
+    ('DT', 'NN'), # the end # That's the end of the story.
+    ('DT', 'RB', 'JJ', 'NN'), # this really good stuff # There's this really good stuff I use called "It's A 10".
+    ('DT', 'RB', 'VB', 'NN'), # a very kicking composite # The vocals are real, thirsty and charming and inject further animation into a very kicking composite. 
+    ('DT', 'VB', 'NN'), # The Flying Man # Sony Pictures has picked up the rights to The Flying Man.
+    ('JJ', 'CD', 'JJ', 'NN'), # first three prime numbers # Sum of first three prime numbers that end with a 9? 
+    ('JJ', 'CD', 'NN'), # Happy 2016 summer # Happy 2016 Summer to the OFLA members and friends.
+    ('JJ', 'CD', 'RB', 'JJ', 'NN'), # First two really windy days # Finally! First two really windy days this spring at Rhine River
+    ('JJ', 'NN'), # Awful Auntie # Awful Auntie is a children's book written by David Walliams and illustrated by Tony Ross.
+    ('JJ', 'RB', 'JJ', 'NN'), # bad, very bad news # Instead of this being good news however, it is bad, very bad news for Nigeria. 
+    ('NN',) # baby # Do not call me baby, I'm over 30.
 }
